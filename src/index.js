f620bcf1f7e1f1ec0e4ae8b3f9188afbf3ab8c1b
jaydip <pjaydip956@gmail.com>
2024-04-02 22:32:40 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import List from './List';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Location/>
+    <Explore/>
     
   </React.StrictMode>
 );
