d6ff4349502f9b4a441f876fa6a162eb53cdc296
jaydip <pjaydip956@gmail.com>
2024-04-02 10:07:16 +0530

    start home

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Home from './home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Home/>
   </React.StrictMode>
 );
 
